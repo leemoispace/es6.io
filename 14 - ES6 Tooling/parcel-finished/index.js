a7270c26b641df331e6d1f69bd1869f58f185114
leemoispace <>
2022-08-12 00:34:01 +0400

    57/77

--- 14 - ES6 Tooling/parcel-finished/index.js
+++ 14 - ES6 Tooling/parcel-finished/index.js
@@ -1,17 +1,17 @@
-import { uniq } from 'lodash';
-import wait from 'waait';
-import { getName } from './src/user';
+import { uniq } from "lodash";
+import wait from "waait";
+import { getName } from "./src/user";
 
-console.log('it works!');
+console.log("it works!");
 
 console.log(uniq([1, 1, 2, 3, 4, 55, 6, 6, 6]));
 
 console.log(getName());
 
 async function go() {
-  console.log('Starting!');
+  console.log("Starting!");
   await wait(2000);
-  console.log('Done!');
+  console.log("Done!");
 }
 
 go();
